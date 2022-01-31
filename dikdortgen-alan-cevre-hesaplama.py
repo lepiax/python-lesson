e8cf68b9b06e5ca437362ad651675eea85730bd6
lepiax <72159636+lepiax@users.noreply.github.com>
2022-01-31 15:32:58 +0300

    Update dikdortgen-alan-cevre-hesaplama.py

--- dikdortgen-alan-cevre-hesaplama.py
+++ dikdortgen-alan-cevre-hesaplama.py
@@ -2,6 +2,6 @@ def hesapla(kisa, uzun):
     alan = kisa * uzun
     cevre = 2 * (kisa * uzun)
     
-    print(f"alan: {alan} cevre: {cevre}")
+    return f"alan: {alan} cevre: {cevre}"
 
-hesapla(5,5)
+print(hesapla(5,5))
